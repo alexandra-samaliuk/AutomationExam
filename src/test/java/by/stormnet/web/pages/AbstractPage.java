e96d27c79e069dd0fcfd52f28d8eec3b64f7081f
= <=>
2020-01-18 21:06:04 +0300

    set cursor

--- src/test/java/by/stormnet/web/pages/AbstractPage.java
+++ src/test/java/by/stormnet/web/pages/AbstractPage.java
@@ -5,6 +5,7 @@ import by.stormnet.automation.core.utils.PauseLenght;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,41 +13,50 @@ import java.util.List;
 
 public class AbstractPage extends FrameworkCore {
     private static WebDriver driver = getInstance();
+    Actions actions = new Actions(driver);
 
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         return driver;
     }
-    public void openUrl(String url){
+
+    public void openUrl(String url) {
         driver.manage().window().maximize();
         driver.get(url);
+
     }
-    public static void waitForElementVisible(By by){
+
+    public static void waitForElementVisible(By by) {
         try {
             WebDriverWait waiter = new WebDriverWait(driver, PauseLenght.MAX.value());
             waiter.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
-        }catch (Throwable e){
+        } catch (Throwable e) {
             System.out.println(e.getMessage());
         }
     }
-    public static void waitForElementClickable(By by){
+
+    public static void waitForElementClickable(By by) {
         try {
-        WebDriverWait waiter = new WebDriverWait(driver, PauseLenght.MAX.value());
-        waiter.until(ExpectedConditions.elementToBeClickable(by));
-        }catch (Throwable e){
+            WebDriverWait waiter = new WebDriverWait(driver, PauseLenght.MAX.value());
+            waiter.until(ExpectedConditions.elementToBeClickable(by));
+        } catch (Throwable e) {
             System.out.println(e.getMessage());
         }
     }
-    public static By getElementBy(String element){
+
+    public static By getElementBy(String element) {
         return By.xpath(element);
     }
 
-    public WebElement getElement(String element){
+    public WebElement getElement(String element) {
         return driver.findElement(By.xpath(element));
     }
 
-    public List<WebElement> getElements(String element){
+    public List<WebElement> getElements(String element) {
         return driver.findElements(By.xpath(element));
     }
 
-
+    public void setCursorOnElement(String locator) {
+        actions.moveToElement(driver.findElement(By.xpath(locator)));
+        actions.perform();
+    }
 }
